69236d5e64bf60baf5944fe0556c63d47127842b
elghoul10 <80488733+elghoul10@users.noreply.github.com>
2024-11-11 10:24:45 +0000

    Delete article directory

--- article/src/main/java/com/example/article/repositories/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.article.repositories;
-
-import com.example.article.entities.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Integer> {
-}
