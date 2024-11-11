21cbd01a4cba4f176f3b78746c944ae8b56c4ee9
elghoul10 <80488733+elghoul10@users.noreply.github.com>
2024-11-11 10:24:38 +0000

    Delete OrdredeTrans directory

--- OrdredeTrans/src/main/java/tn/esprit/Ordre/Repositories/OrdreRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.Ordre.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.Ordre.Entities.Ordre;
-
-public interface OrdreRepository extends JpaRepository<Ordre, Long> {
-}
