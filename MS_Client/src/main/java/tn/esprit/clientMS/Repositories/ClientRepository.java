8a9ef1ae92724eeb430a6750abe4662717fc1214
elghoul10 <80488733+elghoul10@users.noreply.github.com>
2024-11-11 10:24:30 +0000

    Delete MS_Client directory

--- MS_Client/src/main/java/tn/esprit/clientMS/Repositories/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.clientMS.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.clientMS.entities.Client;
-
-public interface ClientRepository extends JpaRepository<Client,Long> {
-}
