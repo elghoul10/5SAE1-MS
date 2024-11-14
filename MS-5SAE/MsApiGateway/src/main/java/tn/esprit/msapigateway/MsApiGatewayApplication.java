7bca5df292777dba44ca25d64317cf4d1450f862
manelwalha <manel.walha@esen.tn>
2024-11-14 20:00:14 +0100

    keyclock MsApiGetway

---
+++ MS-5SAE/MsApiGateway/src/main/java/tn/esprit/msapigateway/MsApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package tn.esprit.msapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayApplication.class, args);
+	}
+
+}
